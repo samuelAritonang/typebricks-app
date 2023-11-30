1a4c14ae371cdf099c0996665582c70323470b41
Samuel Yosua <samuelyosua.otis@gmail.com>
2023-11-30 14:45:26 +0700

    feat: updated create event feature

--- src/app/dashboard/event/page.jsx
+++ src/app/dashboard/event/page.jsx
@@ -1,8 +1,13 @@
-import ButtonDetailComponent from "@/components/ButtonDetail";
 import ModelCreateNewEvent from "@/components/ModalCreateNewEvent";
 
 async function getListEvent() {
-  const res = await fetch("https://eventmakers-api.vercel.app/api/events");
+  const res = await fetch("https://eventmakers-api.vercel.app/api/events", {
+    next: {
+      tags: ['adding_event']  
+    },
+
+    method: "GET"    
+  });
 
   return await res.json();
 }
@@ -11,34 +16,37 @@ export default async function Page() {
   const data = await getListEvent();
 
   return (
-    <div>
-      <ModelCreateNewEvent></ModelCreateNewEvent>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <ModelCreateNewEvent></ModelCreateNewEvent>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.data.map((value, index) => {
-            return (
-              <tr key={index}>
-                <td>{value.name}</td>
-                <td>{value.location}</td>
-                <td>{value.date}</td>
-                <td>
-                  <ButtonDetailComponent
-                    index={value.id}
-                  ></ButtonDetailComponent>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            <table className="min-w-full text-left text-sm font-light">
+              <thead className="border-b bg-white font-medium">
+                <tr>
+                  <th scope="col" className="px-6 py-4">Name</th>
+                  <th scope="col" className="px-6 py-4">Description</th>
+                  <th scope="col" className="px-6 py-4">Location</th>
+                  <th scope="col" className="px-6 py-4">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.data.map((value, index) => {
+                  return (
+                    <tr key={index} className="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+                      <td className="whitespace-nowrap px-6 py-4">{value.name}</td>                
+                      <td className="whitespace-nowrap px-6 py-4">{value.description}</td>
+                      <td className="whitespace-nowrap px-6 py-4">{value.location}</td>
+                      <td className="whitespace-nowrap px-6 py-4">{value.date}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ModalCreateNewEvent.jsx
+++ src/components/ModalCreateNewEvent.jsx
@@ -1,45 +1,104 @@
 "use client"
 
 import { useState } from "react"
+//import { useRouter } from "next/navigation";
+import ActionRevalidate from "@/app/action";
 
 export default function ModelCreateNewEvent() {
-
+    // const router = useRouter()
     const [isShow, setIsShow] = useState(false)
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [location, setLocation] = useState("")
+    const [date, setDate] = useState("")
+    const [error, setError] = useState("")    
+
+    async function createNewEvent() {
+
+        if (name === "") {
+            setError("Title cannot be empty.");
+        }
+        else if (description === "") {
+            setError("Description cannot be empty.");
+        }
+        else if (location === "") {
+            setError("Location cannot be empty.");
+        }
+        else if (date === "") {
+            setError("Date cannot be empty.");
+        }
+        else {
+            const res = await fetch("https://eventmakers-api.vercel.app/api/events", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ 
+                    name: name, 
+                    description: description, 
+                    location: location, 
+                    date: date,
+                    authorId: "d0ef327c-e6c9-4bb3-8fd2-f8b83171f91a"
+                }),
+            });
+
+            // const { data } = await res.json();
+
+            await ActionRevalidate()
+
+            setName("");
+            setDescription("");
+            setLocation("");
+            setDate("");
+            setIsShow(false)            
+        }
+    }
 
     return (
         <>
             <div className="mb-5">
-                <button onClick={()=>setIsShow(true)} className="border bg-red-400 px-5">
-                    Create new event -perbaikan
+                <button onClick={() => setIsShow(true)} className="border-2 rounded-full bg-red-400 px-3 py-1">
+                    Create new event
                 </button>
             </div>
 
-            <div id="default-modal" tabindex="-1" aria-hidden="true" className={ ` ${!isShow?"hidden":"block"} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-                <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div id="default-modal" tabIndex="-1" aria-hidden="true" className={` ${!isShow ? "hidden" : "block"} overflow-y-auto overflow-x-hidden fixed justify-center items-center w-full`}>
+                <div className="relative p-4 w-full h-full">
                     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Terms of Service
+                                Create Your Event
                             </h3>
-                            <button onClick={()=>setIsShow(false)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                            <button onClick={() => setIsShow(false)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <div className="p-4 md:p-5 space-y-4">
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                            </p>
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                            </p>
-                        </div>
-                        <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                            <button data-modal-hide="default-modal" type="button" className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                        </div>
+                        <>
+                            <div className="p-4">
+                                <div className="flex gap-3 mb-2">
+                                    <div><p>Event Name</p></div>
+                                    <div><input onChange={(e) => setName(e.target.value)} value={name} className="border px-2" type="text" ></input></div>
+                                </div>
+                                <div className="flex gap-3 mb-2">
+                                    <div><p>Event Description</p></div>
+                                    <div><textarea rows={3} onChange={(e) => setDescription(e.target.value)} value={description} className="border px-2"></textarea></div>
+                                </div>
+                                <div className="flex gap-3 mb-2">
+                                    <div><p>Location</p></div>
+                                    <div><input onChange={(e) => setLocation(e.target.value)} value={location} className="border px-2" type="text"></input></div>
+                                </div>
+                                <div className="flex gap-3 mb-2">
+                                    <div><p>Event Date</p></div>
+                                    <div><input onChange={(e) => setDate(e.target.value)} value={date} className="border px-2" type="date" ></input></div>
+                                </div>
+
+                                <button className="border-2 px-2 rounded-md" onClick={createNewEvent}>Add</button>
+                                <p className="text-red-500">{error}</p>
+                            </div>
+                        </>
                     </div>
                 </div>
             </div>
