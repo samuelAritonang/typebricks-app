5865aa8a07e9deb52a0bee63e61aa0f082136b6a
Samuel Yosua <samuelyosua.otis@gmail.com>
2023-12-02 10:38:31 +0700

    feat: restore the page.js file to default

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 
-export default function Page(){
-  redirect("/dashboard/event")
+export default function Page() {
+  return <div></div>;
 }
